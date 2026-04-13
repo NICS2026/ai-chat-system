26fc2afab36e809b2ada177f57e668287b814c4a
NICS2026 <nicscomputerskills1@gmail.com>
2026-04-13 17:53:57 +0300

    Add website

--- script.js
+++ script.js
@@ -6,17 +6,19 @@ async function send() {
 
     let chat = document.getElementById("chat");
 
-    chat.innerHTML += `<div class='me'>Wewe: ${message}</div>`;
+    chat.innerHTML += `<div style="color:blue">Wewe: ${message}</div>`;
     input.value = "";
 
     let res = await fetch("/api/chat", {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: {
+            "Content-Type": "application/json"
+        },
         body: JSON.stringify({ message })
     });
 
     let data = await res.json();
 
-    chat.innerHTML += `<div class='ai'>AI: ${data.reply}</div>`;
+    chat.innerHTML += `<div style="color:green">AI: ${data.reply}</div>`;
     chat.scrollTop = chat.scrollHeight;
 }
